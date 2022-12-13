37d1b11a423c679789d8d7b8c9f445ddf8fc37ce
Jason Hua <99191459+ShadowMythz@users.noreply.github.com>
2022-12-13 12:11:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 function App() {
-  
-  
-
- 
-  
   
   function test(){
     /* eslint-disable no-undef */
@@ -26,8 +22,43 @@ function App() {
               if(tab[i].children[7].innerText!="$0.00"){
                 console.log("found");
                 tab[i].style['background-color']="red";
+
+                const [orderNumber, setOrderNumber] = useState(orderNumber)
+                const [date, setDate] = useState(date)
+                const [dasher, setDasher] = useState(dasher)
+                const [cost, setCost] = useState(cost)
+
+                const orderNumber = tab[i].children[0].innerText;
+                const date = tab[i].children[1].innerText;
+                const dasher = tab[i].children[2].innerText;
+                const cost = tab[i].children[3].innerText;
+
+
+                const data= {
+                  "orderNumber":orderNumber,
+                  "date": date,
+                  "dasher":dasher,
+                  "cost": cost
+                }
+
+                const handleSubmit = (e) => {
+                  e.preventDefault();
+
+                  const objt= {orderNumber, date, dasher, cost}
+
+                  axios.post("https://sheet.best/api/sheets/b41c3153-84ed-4ed5-a308-0f02e47df9f2", data)
+                   .then(response =>{
+                  console.log(response) 
+                   })
+                  }
+
+
+
               }
             }
+
+
+            
            
           }
           
@@ -39,9 +70,11 @@ function App() {
   return (
     <div className="App">
       <button onClick={test}>Find Errors</button>
-     
+      <button onClick={handleSubmit}>Write to Sheets</button>
+
     </div>
   );
 }
 
 export default App;
+
