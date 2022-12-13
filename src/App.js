ce5f9553d6993f31e8318d944fdd94ded4bca951
Ayush Rana <ayush.rana@kotuko.it>
2022-12-13 22:44:35 +0545

    connection and write to google sheets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
 function App() {
+
+  const [orderNumber, setOrderNumber] = useState("007")
+  const [date, setDate] = useState("2/10/2052")
+  const [dasher, setDasher] = useState("Random String")
+  const [cost, setCost] = useState("5$")
+
+  const data= {
+    "orderNumber":"009",
+    "date": "2/10/2052",
+    "dasher":"Test 2",
+    "cost": "10$"
+  }
   
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const objt= {orderNumber, date, dasher, cost}
+
+    // you can pass either objt or data after the url, have fun! :)
+    axios.post("https://sheet.best/api/sheets/b41c3153-84ed-4ed5-a308-0f02e47df9f2", data)
+    .then(response =>{
+      console.log(response)
+    })
+  }
 
  
   
@@ -39,6 +62,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={test}>Find Errors</button>
+      <button onClick={handleSubmit}>Write to Sheets</button>
      
     </div>
   );
